7f869d651a213ccb0b6bf2962c5ffb8729e337b1
Unknown <jamurano@yahoo.com>
2020-02-11 10:00:39 -0700

    Enemy Wander and Spawn

---
+++ WhiteBoxMarty2020/Assets/Scripts/EnemyWander.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float rotSpeed = 100f;
+
+    private bool isWandering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+    private Coroutine wanderRoutine;
+
+    public void Update()
+    {
+
+        if (isWandering == false)
+        {
+            wanderRoutine = StartCoroutine(Wander());
+        }
+        if (isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+        }
+
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+        }
+
+        if (isWalking == true)
+        {   
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnEnable()
+    {
+        wanderRoutine = StartCoroutine(Wander());
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log(gameObject.name + " disabled");
+        if (wanderRoutine != null)
+        {
+            StopCoroutine(wanderRoutine);
+        }
+    }
+
+    IEnumerator Wander()
+    {
+
+        int rotTime = Random.Range(1, 3);
+        int rotWait = Random.Range(1, 3);
+        int rotLOrR = Random.Range(0, 3);
+        int walkWait = Random.Range(1, 3);
+        int walkTime = Random.Range(1, 5);
+
+        isWandering = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotWait);
+        if (rotLOrR == 1)
+        {
+
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+        if (rotLOrR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        isWandering = false;
+    }
+}
---
+++ WhiteBoxMarty2020/Assets/Scripts/Spawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public List<GameObject> SpawnPoints;
+    public List<GameObject> Enemies;
+
+    public bool canSpawn;
+    public float spawnTimeMin;
+    public float spawnTimeMax;
+
+
+    private void Start()
+    {
+        canSpawn = true;
+        StartCoroutine(RandSpawn());
+    }
+
+    void SpawnEnemy()
+    {
+        var spawnPointNumber = Random.Range(0, SpawnPoints.Count);
+        var spawnPoint = SpawnPoints[spawnPointNumber];
+        var enemyNumber = Random.Range(0, Enemies.Count);
+        var enemy = Enemies[enemyNumber];
+        Instantiate(enemy, spawnPoint.transform.position, Quaternion.identity);
+    }
+
+    IEnumerator RandSpawn()
+    {
+        while (canSpawn == true)
+        {
+            var spawnTime = Random.Range(spawnTimeMin, spawnTimeMax);
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+
+    public void StopSpawn()
+    {
+        canSpawn = false;
+    }
+}
