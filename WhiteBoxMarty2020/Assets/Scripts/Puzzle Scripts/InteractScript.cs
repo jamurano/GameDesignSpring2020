f18b2719afda410ddc16a1cf2c643f44d915fdb8
Jessica Murano <jamurano@yahoo.com>
2020-03-17 15:28:23 -0600

    Magic Blast and Puzzle

---
+++ WhiteBoxMarty2020/Assets/Scripts/Puzzle Scripts/InteractScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractScript : MonoBehaviour
+{
+    public bool canInteract = false;
+
+    public GameObject interactableObj;
+    public GameObject popUpBox;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Interact")
+        {
+            popUpBox.SetActive(true);
+            interactableObj = other.gameObject;
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        popUpBox.SetActive(false);
+        canInteract = false;
+    }
+    //hello
+    public void Update()
+    {
+        if (canInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            interactableObj.GetComponent<InteractEvent>().interactEvent.Invoke();
+        }
+    }
+}
---
+++ WhiteBoxMarty2020/Assets/Scripts/Puzzle Scripts/PrintToConsole.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    public void PrintSomething(string toPrint)
+    {
+        print(toPrint);
+    }
+}
