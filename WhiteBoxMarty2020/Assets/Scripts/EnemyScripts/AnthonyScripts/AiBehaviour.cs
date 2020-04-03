3f53624697d283eb11390c313063cea0c9d9dde3
Jessica Murano <jamurano@yahoo.com>
2020-04-03 13:50:13 -0600

    Enemy Ai

---
+++ WhiteBoxMarty2020/Assets/Scripts/EnemyScripts/AnthonyScripts/AiBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AiBehaviour : MonoBehaviour
+{
+/*    public AiBrain aiBrainObj;
+    private NavMeshAgent agent;
+    private WaitForFixedUpdate waitObj = new WaitForFixedUpdate();
+    public bool CanRun { get; set; } = true;
+    
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Stop(bool stopped)
+    {
+        agent.isStopped = stopped;
+    }
+    
+    public void Restart()
+    {
+        StartCoroutine(Start());
+    }
+    private IEnumerator Start()
+    {
+        CanRun = true;
+        while (CanRun)
+        {
+            aiBrainObj.Navigate(agent);
+            yield return waitObj;
+        }
+    }*/
+}
---
+++ WhiteBoxMarty2020/Assets/Scripts/EnemyScripts/AnthonyScripts/AiIdle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "Idle", menuName = "Ai/Function/Idle")]
+public class AiIdle : AiBase 
+{
+/*
+	
+	public override void RunAgent(NavMeshAgent agent)
+    {
+		
+    }*/
+}
